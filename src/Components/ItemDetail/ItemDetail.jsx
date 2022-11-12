32f89ccbd278838596d13c289ef93c6f6f375e52
francobss <francobornancini2000@gmail.com>
2022-11-12 01:54:23 -0300

    minor c

--- src/Components/ItemDetail/ItemDetail.jsx
+++ src/Components/ItemDetail/ItemDetail.jsx
@@ -17,15 +17,22 @@ export const ItemDetail = ({item}) => {
 
     return(
         <div className='item-detail-container'>
-            <p className='item-detail__t' style={{width: "100%"}}>ITEM DETAIL</p>
             <div>
-                <img className='item-detail__img' src={item.pictureUrl} alt={item.title}/>
+                <p className='item-detail__t' style={{width: "100%"}}>ITEM DETAIL</p>
+                <div>
+                    <img className='item-detail__img' src={item.pictureUrl} alt={item.title}/>
+                </div>
+                <div className='item-detail__'>
+                    <h4>{item.title}</h4>
+                    <h5>$ {item.price}</h5>
+                </div>
+                <p className='item-detail__showcont'>productos agregados: {contador}</p>
             </div>
-            <div className='item-detail__'>
-                <h4>{item.title}</h4>
-                <h5>$ {item.price}</h5>
+            <div className='item-d-desc'>
+                <p className='desc-tit'>Descripcion</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias possimus deleniti incidunt quaerat fuga rem impedit quo omnis laudantium est cumque, quibusdam, 
+                    fugit pariatur deserunt, culpa dolor voluptas placeat aliquid!</p>
             </div>
-            <p className='item-detail__showcont'>productos agregados: {contador}</p>
             <ItemCount stock={10} inicial={1} onAdd={agregarProd}/>
         </div>
     )
